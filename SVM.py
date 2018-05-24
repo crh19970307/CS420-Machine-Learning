fb151a70502d30ac6aaeadad479081a255ca6b77
xizeroplus <392377379@qq.com>
2018-05-24 10:09:29 +0800

    finalize SVM

--- SVM.py
+++ SVM.py
@@ -9,36 +9,35 @@ from sklearn.model_selection import GridSearchCV
 n_samples = 60000
 fig_w = 45
 
-data = np.fromfile("mnist_train\\mnist_train_data",dtype=np.uint8)
-X_train = data.reshape(n_samples, -1)[: 2000]
-Y_train = np.fromfile("mnist_train\\mnist_train_label",dtype=np.uint8)[: 2000]
+pca_ratio = 0.65
+C = 6
+gamma = 6e-7
 
-X_test = np.fromfile("mnist_test\\mnist_test_data",dtype=np.uint8).reshape(10000, -1)[: 500]
-Y_test = np.fromfile("mnist_test\\mnist_test_label" ,dtype=np.uint8)[: 500]
+data = np.fromfile("mnist_train/mnist_train_data",dtype=np.uint8)
+X_train = data.reshape(n_samples, -1)
+Y_train = np.fromfile("mnist_train/mnist_train_label",dtype=np.uint8)
 
-for pca_ratio in np.arange(0.4, 1.01, 0.1):
-    for C in (10 ** i for i in range(-4, 2, 1)):
-        for gamma in (10 ** j for j in range(-5, 1, 1)):
+X_test = np.fromfile("mnist_test/mnist_test_data",dtype=np.uint8).reshape(10000, -1)
+Y_test = np.fromfile("mnist_test/mnist_test_label" ,dtype=np.uint8)
 
-            file = open('SVM_output.txt', 'a')
-            file.write('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
-            print('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
-            
-            pca = PCA(n_components=pca_ratio)
-            pca.fit(X_train)
-            X_train_pca = pca.transform(X_train)
-            print(X_train_pca.shape)
-            print(pca.n_components_)
-            X_test_pca = pca.transform(X_test)
 
-            clf = svm.SVC(kernel='rbf', C=C, gamma=gamma)
+file = open('SVM_final_output.txt', 'a')
+file.write('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
+print('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
 
-            clf.fit(X_train_pca, Y_train)
+pca = PCA(n_components=pca_ratio)
+pca.fit(X_train)
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
 
-            Y_pred = clf.predict(X_test_pca)
-            #file.write(metrics.classification_report(Y_test, Y_pred))
-            #file.write('\n')
-            file.write(' Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
-            file.write('\n')
-            print('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
-            file.close()
+clf = svm.SVC(kernel='rbf', C=C, gamma=gamma)
+
+clf.fit(X_train_pca, Y_train)
+
+Y_pred = clf.predict(X_test_pca)
+file.write('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
+file.write('\n')
+file.write(metrics.classification_report(Y_test, Y_pred))
+file.write('\n')
+print('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
+file.close()
