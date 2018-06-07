11bfda7b5c16bd999ccb79b99042906280a0b151
xizeroplus <392377379@qq.com>
2018-06-07 23:20:34 +0800

    add report template

---
+++ capsule/dl.py
@@ -0,0 +1,13 @@
+class My_MNIST(torch.utils.data.Dataset):
+    def __init__(self,root,train):
+        if train:
+            self.data=np.fromfile(root+'/mnist_train/'+"mnist_train_data",dtype=np.uint8)
+            self.label = np.fromfile(root+'/mnist_train/'+"mnist_train_label",dtype=np.uint8)
+        else:
+            self.data=np.fromfile(root+'/mnist_test/'+"mnist_test_data",dtype=np.uint8)
+            self.label = np.fromfile(root+'/mnist_test/'+"mnist_test_label",dtype=np.uint8)
+        self.data=self.data.reshape(self.label.shape[0],45,45)
+    def __len__(self):
+        return self.label.shape[0]
+    def __getitem__(self,idx):
+        return torch.from_numpy(np.array([self.data[idx]])).float(),torch.tensor(int(self.label[idx]))
