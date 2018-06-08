4614e8af73961370afd117f3df601479f3bb599b
xizeroplus <392377379@qq.com>
2018-06-08 21:24:51 +0800

    add preprocessing

--- converter.py
+++ converter.py
@@ -1,37 +1,38 @@
 import numpy as np
 from PIL import Image
 import cv2
+import os
 
 
 def save_as_image(data, filename):
-    im = Image.fromarray(data)
-    im.save(filename)
+	im = Image.fromarray(data)
+	im.save(filename)
 
 def save_as_txt(data, filename):
-    np.savetxt(filename, data, fmt='%i')
+	np.savetxt(filename, data, fmt='%i')
 
 
 def dfs_search_block(arr, x, y):
-    vis[x][y] = True
-    if arr[x][y] == 0:
-        return 0
-    b_size = 1
-    for i in range(8):
-        new_x = x + dx[i]
-        new_y = y + dy[i]
-        if new_x >= 0 and new_x < fig_w and new_y >= 0 and new_y < fig_w and arr[new_x][new_y] != 0 \
-        and not vis[new_x][new_y]:
-            b_size += dfs_search_block(arr, new_x, new_y)
-    return b_size
+	vis[x][y] = True
+	if arr[x][y] == 0:
+		return 0
+	b_size = 1
+	for i in range(8):
+		new_x = x + dx[i]
+		new_y = y + dy[i]
+		if new_x >= 0 and new_x < fig_w and new_y >= 0 and new_y < fig_w and arr[new_x][new_y] != 0 \
+		and not vis[new_x][new_y]:
+			b_size += dfs_search_block(arr, new_x, new_y)
+	return b_size
 
 
 def dfs_remove_block(arr, x, y):
-    arr[x][y] = 0
-    for i in range(8):
-        new_x = x + dx[i]
-        new_y = y + dy[i]
-        if new_x >= 0 and new_x < fig_w and new_y >= 0 and new_y < fig_w and arr[new_x][new_y] != 0:
-            dfs_remove_block(arr, new_x, new_y)
+	arr[x][y] = 0
+	for i in range(8):
+		new_x = x + dx[i]
+		new_y = y + dy[i]
+		if new_x >= 0 and new_x < fig_w and new_y >= 0 and new_y < fig_w and arr[new_x][new_y] != 0:
+			dfs_remove_block(arr, new_x, new_y)
 
 
 n_samples = 60000
@@ -55,46 +56,55 @@ images = np.zeros((10000, 45 * 45), dtype=np.uint8)
 
 for id in range(10000):
 
-    image = data[id]
-    # save_as_image(image, 'original.png')
+	id = 30
 
-    arr = np.asarray(image)
-    # save_as_txt(arr, 'original.txt')
+	image = data[id]
+	save_as_image(image, str(id) + '_original.png')
 
-    vis = [[False for i in range(fig_w)] for j in range(fig_w)]
+	arr = np.asarray(image)
 
-    for i in range(fig_w):
-        for j in range(fig_w):
-            if vis[i][j]:
-                continue
-            block_size = dfs_search_block(arr, i, j)
-            if block_size > 0 and block_size <= 20:
-                dfs_remove_block(arr, i, j)
-                # print("Remove block of size ", block_size)
+	vis = [[False for i in range(fig_w)] for j in range(fig_w)]
 
-    sum_rows = np.sum(arr, axis=1)
-    upper = np.where(sum_rows > 0)[0][0]
-    lower = np.where(sum_rows > 0)[0][-1]
+	for i in range(fig_w):
+		for j in range(fig_w):
+			if vis[i][j]:
+				continue
+			block_size = dfs_search_block(arr, i, j)
+			if block_size > 0 and block_size <= 20:
+				dfs_remove_block(arr, i, j)
+				# print("Remove block of size ", block_size)
 
-    sum_cols = np.sum(arr, axis=0)
-    left = np.where(sum_cols > 0)[0][0]
-    right = np.where(sum_cols > 0)[0][-1]
+	save_as_image(arr, str(id) + '_denoised.png')
 
-    top_padding = (45 - lower + upper - 1) // 2
-    bottom_padding = 45 - (top_padding + lower - upper + 1)
+	sum_rows = np.sum(arr, axis=1)
+	upper = np.where(sum_rows > 0)[0][0]
+	lower = np.where(sum_rows > 0)[0][-1]
 
-    left_padding = (45 - right + left - 1) // 2
-    right_padding = 45 - (left_padding + right - left + 1)
-    # print(top_padding, bottom_padding, left_padding, right_padding)
+	sum_cols = np.sum(arr, axis=0)
+	left = np.where(sum_cols > 0)[0][0]
+	right = np.where(sum_cols > 0)[0][-1]
 
-    arr = arr[upper: lower + 1, left: right +1]
-    arr = cv2.copyMakeBorder(arr, top_padding, bottom_padding, left_padding, right_padding, cv2.BORDER_CONSTANT,value=0)
+	top_padding = (45 - lower + upper - 1) // 2
+	bottom_padding = 45 - (top_padding + lower - upper + 1)
 
-    arr = arr.reshape(1, 45 * 45)
-    images[id] = arr
-    if id % 100 == 0:
-        print(id, ' done')
-    
+	left_padding = (45 - right + left - 1) // 2
+	right_padding = 45 - (left_padding + right - left + 1)
+	# print(top_padding, bottom_padding, left_padding, right_padding)
+
+	arr = arr[upper: lower + 1, left: right +1]
+
+	save_as_image(arr, str(id) + '_cropped.png')
+
+	arr = cv2.copyMakeBorder(arr, top_padding, bottom_padding, left_padding, right_padding, cv2.BORDER_CONSTANT,value=0)
+	save_as_image(arr, str(id) + '_centered.png')
+
+	os._exit(0)
+
+	arr = arr.reshape(1, 45 * 45)
+	images[id] = arr
+	if id % 100 == 0:
+		print(id, ' done')
+	
 images.tofile('new_test_data')
 
 
