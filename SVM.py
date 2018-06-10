25ce6cf5dd8cdc675de4c8e69860bbab31e8fd52
xizeroplus <392377379@qq.com>
2018-06-10 19:34:02 +0800

    add capsule report

--- SVM.py
+++ SVM.py
@@ -20,27 +20,24 @@ Y_train = np.fromfile("mnist_train/mnist_train_label",dtype=np.uint8)
 X_test = np.fromfile("mnist_test/new_test_data",dtype=np.uint8).reshape(10000, -1)
 Y_test = np.fromfile("mnist_test/mnist_test_label" ,dtype=np.uint8)
 
-for pca_ratio in np.arange(0.95, 0.96, 0.05):
-	for C in [5, 10]:
-		for gamma in [2e-7, 5e-7, 1e-6]:
 
-			file = open('SVMwith_output2.txt', 'a')
-			file.write('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
-			print('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
+file = open('SVMwith_output2.txt', 'a')
+file.write('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
+print('pca_ratio = ' + str(pca_ratio) + '  C = ' + str(C) + '  gamma = ' + str(gamma))
 
-			pca = PCA(n_components=pca_ratio)
-			pca.fit(X_train)
-			X_train_pca = pca.transform(X_train)
-			X_test_pca = pca.transform(X_test)
+pca = PCA(n_components=pca_ratio)
+pca.fit(X_train)
+X_train_pca = pca.transform(X_train)
+X_test_pca = pca.transform(X_test)
 
-			clf = svm.SVC(kernel='rbf', C=C, gamma=gamma)
+clf = svm.SVC(kernel='rbf', C=C, gamma=gamma)
 
-			clf.fit(X_train_pca, Y_train)
+clf.fit(X_train_pca, Y_train)
 
-			Y_pred = clf.predict(X_test_pca)
-			file.write('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
-			file.write('\n')
-			# file.write(metrics.classification_report(Y_test, Y_pred))
-			file.write('\n')
-			print('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
-			file.close()
+Y_pred = clf.predict(X_test_pca)
+file.write('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
+file.write('\n')
+# file.write(metrics.classification_report(Y_test, Y_pred))
+file.write('\n')
+print('Accuracy = ' + str(metrics.accuracy_score(Y_test, Y_pred)))
+file.close()
