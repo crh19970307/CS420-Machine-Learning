612944733ef7880aae83e764c7badbf3f22ec5c1
Ruiheng Chang <995494637@qq.com>
2018-06-12 21:56:51 +0800

    add simplecnn

---
+++ deep-learning-method/lib/nets/SimpleCNN.py
@@ -0,0 +1,31 @@
+import torch 
+import torch.nn as nn
+import numpy as np
+import torchvision
+import os
+
+# Convolutional neural network (two convolutional layers)
+class ConvNet(nn.Module):
+    def __init__(self, num_classes=10):
+        super(ConvNet, self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 16, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(16, 32, kernel_size=5, stride=1, padding=2),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2))
+        self.fc = nn.Linear(11*11*32, num_classes)
+        
+    def forward(self, x):
+        #print(x.shape)
+        out = self.layer1(x)
+        #print(out.shape)
+        out = self.layer2(out)
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+        return out
